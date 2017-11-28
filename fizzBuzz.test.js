9a10e6c7638466b5d9ad91409dc30d2d52dbb4e4
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-11-28 20:13:38 +0100

    Initial commit

---
+++ fizzBuzz.test.js
@@ -0,0 +1,28 @@
+/* global define, it, describe */
+const should = require('should')
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// 1 - 15
+// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
+
+
+
+
+function fizzBuzzTal (tal) {
+  return '1'
+}
+
+describe('tal för tal ', () => {
+  it('1 blir 1', () => fizzBuzzTal(1).should.equal('1'))
+})
+
+
+
+
+
+// function fizzBuzzMellanTal(från, till){
+//   return ''
+// }
+
+// describe('Lista av tal', () => {
+//   it('från 1 till 1', () => fizzBuzzMellanTal(1, 1).should.equal('1'))
+// })
