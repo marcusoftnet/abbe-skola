ec2ab7c22f7e9d7834d8d6effc6fd74100e17596
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-11-30 21:00:06 +0100

    Starting point with complete solution

--- fizzBuzzer.js
+++ fizzBuzzer.js
@@ -1,20 +1,58 @@
-
 function tal (tal) {
-  if(tal % 3 === 0 && tal % 5 === 0) return 'FizzBuzz'
-  if(tal % 3 === 0) return 'Fizz'
-  if(tal % 5 === 0) return 'Buzz'
-  return tal.toString()
+  return ''
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function mellanTal (från, till) {
-  let svarslista = []
-  for(let i=från; i <= till; i++){
-    let talet = tal(i)
-    svarslista.push(talet)
-  }
-  let svaret = svarslista.join(',')
-  return svaret
+  return ''
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
