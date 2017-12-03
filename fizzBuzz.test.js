9d0a13cc72ea36301767d8192921e1661f6d15ae
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-12-03 20:49:33 +0100

    Clean up prep

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -14,7 +14,7 @@ const fizzBuzz = require('./fizzBuzzer')
 
 
 describe('tal för tal - ', () => {
-  it('1 ska bli 1', () => { fizzBuzz.tal(1).should.equal('???') })
+  it('1 ska bli 1', () => { fizzBuzz.tal(1).should.equal('1') })
 })
 
 
@@ -58,9 +58,9 @@ describe('tal för tal - ', () => {
 
 
 
-describe('Från ett tal till ett annat - ', () => {
-  it('från 1 till 1', () => fizzBuzz.mellanTal(1, 1).should.equal('?'))
-})
+// describe('Från ett tal till ett annat - ', () => {
+//   it('från 1 till 1', () => fizzBuzz.mellanTal(1, 1).should.equal('1'))
+// })
 
 // say.speak(fizzBuzz.mellanTal(1, 15))
 // say.speak(fizzBuzz.mellanTal(1, 10000), 'Samantha', 4)
